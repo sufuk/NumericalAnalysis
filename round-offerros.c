6edae2e0c5a8949f95fd105bdeae90a6c4f1fdef
Semih Ufuk Güler <sufukguler@gmail.com>
2020-04-10 18:08:05 +0300

    Update round-offerros.c

--- round-offerros.c
+++ round-offerros.c
@@ -23,7 +23,37 @@ for(i = 1 ; i < 1000 ; i++){
 
 }
 
-printf("%f", y);
+printf("%f\n", y);
+
+
+double k1 = 1;
+double n1 = 100000000;
+double form = 0;
+
+for(k1 ; k1 <= n1 ; k1++){
+
+
+form += 1 / (k1 * k1) ;
+
+
+
+}
+
+printf("%lf\n", form);
+
+double k2 = 100000000;
+double n2 = 1;
+double sum = 0;
+for(k2 ; k2 >= n2 ; k2--){
+
+sum += 1 / (k2 * k2);
+
+}
+
+printf("%lf\n", sum);
+
+
+
 
 
   return 0;
