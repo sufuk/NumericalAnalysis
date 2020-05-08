8a16a44aa667ca0f151a25434b5a3c0c2993f892
Semih Ufuk Güler <sufukguler@gmail.com>
2020-05-08 22:05:44 +0300

    Update HW2.1.c

--- HW2.1.c
+++ HW2.1.c
@@ -13,6 +13,7 @@ x = 1.37;
 char a[10];
 y = pow(x, 3) - 7*(pow(x, 2)) + 8*x - 0.35;
 y0 = y;
+printf("QUESTION 1 (a):\n");
 printf("True Value is: %lf\n", y0);
 sprintf(a, "%.3f", y);
 sscanf(a, "%f", &y);
@@ -23,10 +24,22 @@ float relative_error = (true_error / y );
 printf("Relative Error is: %f\n", relative_error);
 float percent_error = (true_error / y) * 100;
 printf("Percent Error is %% %f\n", percent_error);
-
+printf("\n\n");
 
 ///y = ((x − 7)x + 8)x − 0.35 at x =1.37
-
-
+char b[10];
+float y1 = (((x - 7) * x) + 8)*x - 0.35;
+float y2 = y1;
+printf("QUESTION 1 (b):\n");
+printf("True Value is: %f\n", y1);
+sprintf(b, "%.3f", y1);
+sscanf(b, "%f", &y1);
+printf("Chopped Value is: %f\n", y1);
+float true_error1 = (y2 - y1);
+printf("True Error is: %f\n", true_error1);
+float relative_error1 = (true_error1 / y1 );
+printf("Relative Error is: %f\n", relative_error1);
+float percent_error1 = (true_error1 / y1) * 100;
+printf("Percent Error is %% %f\n", percent_error1);
   return 0;
 }
