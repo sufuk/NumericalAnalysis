e28b5878d8e895be25075103263f1746f2c37328
Semih Ufuk Güler <sufukguler@gmail.com>
2020-05-10 12:33:27 +0300

    Create HW2.2.c

---
+++ HW2.2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int maclaurin_e(int x ){
+
+int x;
+
+
+
+
+
+
+  return n;
+}
+
+int main(){
+
+int reuslt = exp(15);
+printf("%d", result)
+
+  return 0;
+}
