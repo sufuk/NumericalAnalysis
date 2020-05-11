a851ff739d298afb509379f57a28ce13e1e184b2
Semih Ufuk Güler <sufukguler@gmail.com>
2020-05-11 06:51:19 +0300

    Update HW2.3.c

--- HW2.3.c
+++ HW2.3.c
@@ -22,18 +22,35 @@ y01 = (150*x0) - 12; //y"
 y02 = 150 ;//y"'
 y03 = 0; //y""
 y04 = 0; //y""'
-double ts; //taylor series
-ts = (y001) + (y00*(x-x0)) + ((y01*(pow((x-x0), 2)))/2) + ((y02*(pow((x-x0), 3)))/6)
-+ ((y03*(pow((x-x0), 4)))/24) + ((y04*(pow((x-x0), 5)))/120);
+double ts0, ts1, ts2, ts3, ts4, ts; //taylor series
+float exact = 554;
 printf("y  : %lf\n", y);
 printf("y0 : %lf\n", y0);
 printf("y1 : %lf\n", y1);
 printf("y2 : %lf\n", y2);
 printf("y3 : %lf\n", y3);
 printf("y4 : %lf\n", y4);
-
-
-printf("ts :%f\n", ts);
-
+ts = (y001) + (y00*(x-x0)) + ((y01*(pow((x-x0), 2)))/2) + ((y02*(pow((x-x0), 3)))/6)
++ ((y03*(pow((x-x0), 4)))/24) + ((y04*(pow((x-x0), 5)))/120);
+ts0 = (y001);
+ts1 = (y001) + (y00*(x-x0));
+ts2 = (y001) + (y00*(x-x0)) + ((y01*(pow((x-x0), 2)))/2);
+ts3 = (y001) + (y00*(x-x0)) + ((y01*(pow((x-x0), 2)))/2) + ((y02*(pow((x-x0), 3)))/6);
+ts4 = (y001) + (y00*(x-x0)) + ((y01*(pow((x-x0), 2)))/2) + ((y02*(pow((x-x0), 3)))/6) + ((y03*(pow((x-x0), 4)))/24);
+printf("n =0, ts :%f\n", ts0);
+float e0 = ((exact-ts0)/exact)*100 ;
+printf("Error is%%%f\n",e0);
+printf("n =1, ts :%f\n", ts1);
+float e1 = ((exact-ts1)/exact)*100 ;
+printf("Error is%%%f\n",e1);
+printf("n =2, ts :%f\n", ts2);
+float e2 = ((exact-ts2)/exact)*100 ;
+printf("Error is%%%f\n",e2);
+printf("n =3, ts :%f\n", ts3);
+float e3 = ((exact-ts3)/exact)*100 ;
+printf("Error is%%%f\n",e3);
+printf("n =4, ts :%f\n", ts4);
+float e4 = ((exact-ts4)/exact)*100 ;
+printf("Error is%%%f\n",e4);
   return 0;
 }
